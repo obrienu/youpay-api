7c3086045e8cde5ef34a427820967c4a5519e717
obrienu <obrienules@gmail.com>
2020-11-17 23:45:09 +0100

    Adds User, BankingDetails, Transaction table

---
+++ Migrations/20201117223741_AddsUsersTransactionAndAccount.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Youpay.API.Migrations
+{
+    public partial class AddsUsersTransactionAndAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Firstname = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Sex = table.Column<int>(type: "INTEGER", maxLength: 6, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    PasswordResetToken = table.Column<string>(type: "TEXT", maxLength: 6, nullable: true),
+                    ResetExpiresAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "GETDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BankingDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BankName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    BankNumber = table.Column<long>(type: "INTEGER", nullable: false),
+                    AccountType = table.Column<int>(type: "INTEGER", maxLength: 20, nullable: false),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "GETDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankingDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BankingDetails_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    ProductName = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Category = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Charges = table.Column<int>(type: "INTEGER", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    HasPaid = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false),
+                    IsCanceled = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false),
+                    HasShipped = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false),
+                    ConfirmedDilivery = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false),
+                    Completed = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false),
+                    HasIssue = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false),
+                    MerchantId = table.Column<int>(type: "INTEGER", nullable: true),
+                    BuyerId = table.Column<int>(type: "INTEGER", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "GETDATE()"),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Users_BuyerId",
+                        column: x => x.BuyerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Users_MerchantId",
+                        column: x => x.MerchantId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankingDetails_UserId",
+                table: "BankingDetails",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_BuyerId",
+                table: "Transactions",
+                column: "BuyerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_MerchantId",
+                table: "Transactions",
+                column: "MerchantId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankingDetails");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
