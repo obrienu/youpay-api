7eebf3cd165f34eb60d21f4a221cb2f360619b0f
obrienu <obrienules@gmail.com>
2020-11-17 23:42:58 +0100

    Adds Banking details model

---
+++ Models/BankingDetails.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Youpay.API.Helpers;
+
+namespace Youpay.API.Models
+{
+    public class BankingDetails
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        [Required]
+        [StringLength(100)]
+        public string BankName { get; set; }
+        [Required]
+        public long BankNumber { get; set; }
+        [Required]
+        [StringLength(20)]
+        public AccountType AccountType { get; set; }
+        [Required]
+        public User User { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]        
+        public DateTime UpdatedAt { get; set; }
+    }
+}
