14d41bce93b89db287d32010eab8ea27ba09d51d
obrienu <obrienules@gmail.com>
2020-11-17 23:42:45 +0100

    Adds Transactions model

---
+++ Models/Transaction.cs
@@ -0,0 +1,36 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Youpay.API.Models
+{
+    public class Transaction
+    {
+        [Key, Required, StringLength(50), DatabaseGenerated(DatabaseGeneratedOption.None), ]
+        public string Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string ProductName { get; set; }
+        [Required, StringLength(100)]
+        public string Category { get; set; }
+        [Required]
+        public int Charges { get; set; }
+
+        [Required, StringLength(255)]
+        public string Description { get; set; }
+        public bool HasPaid { get; set; }
+        public bool IsCanceled { get; set; }
+        public bool HasShipped { get; set; }
+        public bool ConfirmedDilivery { get; set; }
+        public bool Completed { get; set; }
+        public bool HasIssue { get; set; }
+        public User Merchant { get; set; }
+        public User Buyer { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]        
+        public DateTime UpdatedAt { get; set; }
+    }
+}
