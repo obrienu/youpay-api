0fdf3c5602815da14896670d8c7e10f1c4b80e5e
obrienu <obrienules@gmail.com>
2020-11-18 11:28:00 +0100

    Adds Token and Transaction to services

--- Startup.cs
+++ Startup.cs
@@ -1,25 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Youpay.API.Data;
 using Youpay.API.Helpers;
+using Youpay.API.Utils;
+using Youpay.API.Utils.Impl;
 
 namespace Youpay.API
 {
@@ -35,7 +30,8 @@ namespace Youpay.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<ITokenUtil, TokenUtil>();
+            services.AddScoped<ITransactionsUtil, TransactionsUtil>();
             services.AddDbContext<DataContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
