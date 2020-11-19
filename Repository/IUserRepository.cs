59d9aef8f6c5a075d450afe49b1d0fbe2e06d1f0
obrienu <obrienules@gmail.com>
2020-11-19 09:08:17 +0100

    Adds UserRepository

---
+++ Repository/IUserRepository.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Youpay.API.Models;
+
+namespace Youpay.API.Repository.Impl
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUser(long id);
+        Task<List<User>> GetUsers();
+        Task<User> FindUserByEmail(string email);
+        void SaveUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(long id);
+        Task<bool> SaveChanges();
+        Task<bool> UserExists(string email);
+
+    }
+}
---
+++ Repository/Impl/UserRepository.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Youpay.API.Data;
+using Youpay.API.Models;
+
+namespace Youpay.API.Repository.Impl
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+
+        }
+        public void DeleteUser(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<User> FindUserByEmail(string email)
+        {
+            var user = await _context.Users.Include(user => user.BankingDetails)
+                .FirstOrDefaultAsync(user => user.Email == email);
+            return user;
+        }
+
+        public async Task<User> GetUser(long id)
+        {
+            var userToFind = await _context.Users.Include(user => user.BankingDetails)
+                .FirstOrDefaultAsync(user => user.Id == id);
+                return userToFind;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+        public async Task<bool> SaveChanges()
+        {
+           return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async void SaveUser(User user)
+        {
+            await _context.Users.AddAsync(user);
+        }
+
+        public void UpdateUser(User user)
+        {
+             _context.Update(user);
+        }
+
+        public async Task<bool> UserExists(string email)
+        {
+            if(await _context.Users.AnyAsync(user => user.Email.Equals(email)))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
