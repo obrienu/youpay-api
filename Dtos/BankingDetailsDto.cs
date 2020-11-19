a65451cfd43296201d252eed697df40bb258cd12
obrienu <obrienules@gmail.com>
2020-11-19 09:16:59 +0100

    Adds BankingDetailsDto

---
+++ Dtos/BankingDetailsDto.cs
@@ -0,0 +1,13 @@
+using Youpay.API.Helpers;
+
+namespace Youpay.API.Dtos
+{
+    public class BankingDetailsDto
+    {
+        public long? Id { get; set; }
+        public string BankName { get; set; }
+        public long BankNumber { get; set; }
+        public AccountType AccountType { get; set; }
+        public bool IsMain { get; set; }
+    }
+}
