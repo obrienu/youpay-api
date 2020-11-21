6b5b3530a6e3be0d2c264ed698eadf47866058ef
obrienu <obrienules@gmail.com>
2020-11-21 17:33:28 +0100

    Enforces required validation on all fields

--- Dtos/UserLoginDto.cs
+++ Dtos/UserLoginDto.cs
@@ -4,9 +4,9 @@ namespace Youpay.API.Dtos
 {
     public class UserLoginDto
     {
-        [EmailAddress]
+        [Required, EmailAddress]
         public string Email { get; set; }
-        [MinLength(8)]
+        [Required, MinLength(8)]
         public string Password { get; set; }
     }
 }
