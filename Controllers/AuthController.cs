4d735ae187efcfcfbb9a3345ef3ef8c0d41ff0ad
obrienu <obrienules@gmail.com>
2020-11-21 17:06:39 +0100

    Removes unused imports

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,10 +1,5 @@
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Youpay.API.Dtos;
 using Youpay.API.Services.Impl;
 
