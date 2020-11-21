4c82d3ace8cbc7ec129a1ac60bd72f6c678dc1d1
obrienu <obrienules@gmail.com>
2020-11-21 17:30:52 +0100

    Changes BankingDetails to BankingDetailsDto

--- Dtos/UserDto.cs
+++ Dtos/UserDto.cs
@@ -10,11 +10,11 @@ namespace Youpay.API.Dtos
         public long? Id { get; set; }
         public string Firstname { get; set; }
         public string LastName { get; set; }
-        public Gender Sex { get; set; }
+        public string Sex { get; set; }
         public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
-        public virtual ICollection<BankingDetails> BankingDetails { get; set; }
+        public virtual ICollection<BankingDetailsDto> BankingDetails { get; set; }
 
     }
 }
