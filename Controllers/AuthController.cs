857d08f7fb915ef9186d058caa9bafd4dd00c2fd
obrienu <obrienules@gmail.com>
2020-11-22 22:14:01 +0100

    Adds route for first time user transaction creation

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Youpay.API.Dtos;
+using Youpay.API.Services;
 using Youpay.API.Services.Impl;
 
 namespace Youpay.API.Controllers
@@ -10,13 +11,15 @@ namespace Youpay.API.Controllers
     public class AuthController : Controller
     {
         private readonly IAuthServices _authServices;
-        public AuthController(IAuthServices authServices)
+        private readonly ITransactionServices _tranService;
+        public AuthController(IAuthServices authServices, ITransactionServices tranService)
         {
+            _tranService = tranService;
             _authServices = authServices;
         }
 
         [HttpPost("login")]
-        public async Task<IActionResult> Login( UserLoginDto userLoginDto)
+        public async Task<IActionResult> Login(UserLoginDto userLoginDto)
         {
             var response = await _authServices.Login(userLoginDto);
             return StatusCode(response.Status, response);
@@ -32,15 +35,22 @@ namespace Youpay.API.Controllers
         [HttpPost("request-password-reset")]
         public async Task<IActionResult> RequestPasswordReset([FromBody] RequestPasswordResetDto requestPasswordResetDto)
         {
-            var response =  await _authServices.RequestPasswordReset(requestPasswordResetDto.Email);
+            var response = await _authServices.RequestPasswordReset(requestPasswordResetDto.Email);
             return StatusCode(response.Status, response);
         }
 
         [HttpPost("reset-password")]
         public async Task<IActionResult> ResetPassword([FromBody] PasswordResetDto passwordResetDto)
         {
-            var response =  await _authServices.ResetPassword(passwordResetDto);
-             return StatusCode(response.Status, response);
+            var response = await _authServices.ResetPassword(passwordResetDto);
+            return StatusCode(response.Status, response);
+        }
+
+        [HttpPost("first-time-transaction")]
+        public async Task<ActionResult> FirstTimeTransactionRegistration([FromBody] FirstTimeUserTransactionRegisterationDto registerationDto)
+        {
+            var response = await _tranService.AddFirstTimeUserTransaction(registerationDto);
+            return StatusCode(response.Status, response);
         }
 
     }
